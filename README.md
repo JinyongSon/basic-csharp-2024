839e0b40efcf6befb0f37351559681a85b746a79
JinyongSon <wlsdydths@naver.com>
2024-04-15 09:47:36 +0900

    3일차

--- README.md
+++ README.md
@@ -167,7 +167,7 @@
     - 클래스는 상속 시 별다른 문제없음 vs 인터페이스는 구현을 하면 약속을 지키지 않으면 오류표시
     - 인터페이스에서 약속한 메서드를 구현안하면 빌드 안됨!
 
-    ![인터페이스설명](https://raw.githubusercontent.com/hugoMGSung/basic-csharp-2024/main/images/cs001.png)
+    ![인터페이스설명](https://raw.githubusercontent.com/JinyongSon/basic-csharp-2024/main/images/cs001.png)
 
 - 추상클래스(abstract)
     - Virutal 메서드하고도 유사
@@ -231,7 +231,7 @@
 - TIP, C# 주석 중 영역을 지정할 수 있는 주석
     - #region ~ #endregion 영역을 Expend 또는 Collapse 가능
 
-    ![region주석](https://raw.githubusercontent.com/hugoMGSung/basic-csharp-2024/main/images/cs002.png)
+    ![region주석](https://raw.githubusercontent.com/JinyongSon/basic-csharp-2024/main/images/cs002.png)
 
 ## 3일차
 - 람다식 
--- day03/cs03_basic_app/ex12_lambdas/Program.cs
+++ day03/cs03_basic_app/ex12_lambdas/Program.cs
@@ -1,10 +1,33 @@
 ﻿namespace ex12_lambdas
 {
+    // 대리자의 정수값 두개 받아서 정수값을 리턴해주는 함수들은 내가 대신 실행시켜줄게
+    delegate int Calculate(int a, int b);
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("익명 메서드");
+
+            Calculate calc = delegate (int a, int b) {
+                return a + b;
+            };
+
+            Console.WriteLine($"계산결과 = {calc(10, 4)}");
+
+            // 람다식을 쓰면 훨씬 짧게 코딩가능
+            // calc와 동일한 기능
+            Calculate calc2 = (a, b) => a + b; // (int a, int b) => { return a + b; };
+
+            Console.WriteLine($"계산결과 = {calc2(11, 4)}");
+
+            // 문장형식의 람다식
+            Calculate calc3 = (a, b) =>
+            {
+                Console.WriteLine("이런식으로 뺄셈이 됩니다.");
+                var sum = a - b;
+                return sum;
+            };
+            Console.WriteLine($"계산결과 = {calc3(11, 4)}");
         }
     }
 }
